e1ae8db03769a4981f0e00be334b765fa4a24bc7
Ivan Schwindt <ynventa@hotmail.com>
2017-12-04 16:20:48 -0300

    Install redux, react-bootstrap. Design view.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Navbar, Grid, Row, Col, FormGroup, ControlLabel, Button, FormControl, Form } from 'react-bootstrap';
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Navbar inverse staticTop>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#">Simple Chat</a>
+            </Navbar.Brand>
+          </Navbar.Header>
+        </Navbar>
+
+        <Grid>
+          <Row>
+            <Col sm={8}>
+              <div class="show-messages">
+              </div>
+            </Col>
+            <Col sm={8}>
+
+              <Form inline>
+    
+                <FormGroup controlId="formInlineEmail">
+                  <FormControl type="text" placeholder="Enter text" />
+                </FormGroup>
+                {' '}
+                <Button bsStyle="primary">
+                  Send
+                </Button>
+              </Form>
+            </Col>
+          </Row>
+        </Grid>
       </div>
     );
   }
